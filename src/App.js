651837f424f069785146d239d98b4868c21abbc7
SALMA EL BARBORI <73172522+salmaelbarbori@users.noreply.github.com>
2023-08-14 11:45:05 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import { useState, useRef, useEffect } from 'react'
+import Cards from './Components/Cards'
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <h1>Memory Game choose from 1 to 18</h1>
+      <Cards />
+    </div>
+  );
+}
+
+
+export default App;
